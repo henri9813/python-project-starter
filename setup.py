6e4221438abc7d8336de03621fcadab67eae31fe
henri9813 <henri.devigne@bonkgaming.fr>
2019-01-16 17:05:36 +0100

    Lint setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,30 @@
+"""
+Module installation script
+"""
+import os
 from setuptools import setup, find_packages
-from os import path
 
-with open(path.join(path.abspath(path.dirname(__file__)), 'README.md')) as f:
-    long_description = f.read()
+
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')) as f:
+    LONG_DESCRIPTION = f.read()
+
 
 def package_files(directory):
+    """
+    Get all files/directories in a directory
+    :param directory: path to analyse
+    :type directory: str
+    :return: list of all files/directories
+    :rtype: list
+    """
     paths = []
-    for (path, directories, filenames) in os.walk(directory):
+    for (path, _, filenames) in os.walk(directory):
         for filename in filenames:
-            paths.append(os.path.join('..', path, filename))
+            paths.append(path.join('..', path, filename))
     return paths
 
 
-extra_files = package_files('python_project_starter/template')
+EXTRA_FILES = package_files('python_project_starter/template')
 
 
 setup(
@@ -22,13 +34,13 @@ setup(
     url='https://github.com/henri9813/python-project-starter',
     version='1.0',
     description="This package permit you to easily create a new project",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     entry_points={
         'console_scripts': [
             'python-project-starter=python_project_starter.__main__:main'
         ]
     },
-    package_data={'': extra_files},
+    package_data={'': EXTRA_FILES},
     include_package_data=True
 )
