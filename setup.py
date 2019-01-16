31b6e0a8ae9d447dfc8f075244f292c52ed06341
henri9813 <henri.devigne@bonkgaming.fr>
2019-01-16 15:35:16 +0100

    Add template and update setup.py to include these files

--- setup.py
+++ setup.py
@@ -4,6 +4,17 @@ from os import path
 with open(path.join(path.abspath(path.dirname(__file__)), 'README.md')) as f:
     long_description = f.read()
 
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+extra_files = package_files('python_project_starter/template')
+
+
 setup(
     name='python-project-starter',
     author='Henri Devigne',
@@ -17,5 +28,7 @@ setup(
         'console_scripts': [
             'python-project-starter=python_project_starter.__main__:main'
         ]
-    }
+    },
+    package_data={'': extra_files},
+    include_package_data=True
 )
